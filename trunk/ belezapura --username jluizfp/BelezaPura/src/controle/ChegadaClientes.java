2bbfa32c565cd1b72c720a222bc6f9aa398f78d1
fgalluzzo <fgalluzzo@b0fe7432-4499-11de-952f-710615d9d0d5>
2009-06-01 14:08:27 +0000

    Criei a thread de chegada de clientes, criei uma PriorityQueue de clientes, alterei a GeraServico para ser static, alterei o simulador para usar a Thread.

---
+++ trunk/ belezapura --username jluizfp/BelezaPura/src/controle/ChegadaClientes.java
@@ -0,0 +1,46 @@
+package controle;
+
+import modelo.Cliente;
+import util.GeraServico;
+import view.simulador;
+
+public class ChegadaClientes extends Thread {
+	private final double lambda = 0.3;
+	
+	public void run(){
+		while((System.currentTimeMillis()- simulador.tempoInicial) < 10000){
+			
+			//chegadas exponencial, mas podemos usar tb o que foi especificado
+			//de chegadas entre 1 e 5 unidades de tempo
+			double tempoEntreChegadas =  (- Math.log(Math.random())/lambda)*1000;
+			
+		    //Instancio um novo cliente
+			Cliente c = new Cliente();
+			
+			//Atribuo serviços aleatórios ao cliente
+			c.setServicos(GeraServico.gerador());
+			//Atribuo o tempo de chegada dele
+			c.setTempoChegada(System.currentTimeMillis() - simulador.tempoInicial);
+			
+			synchronized (simulador.fila) {
+				//Insiro o cliente na fila de espera
+				simulador.fila.insereCliente(c);
+			}			
+			//Checagem. Pode apagar depois
+		    System.out.println(c.getTempoChegada());
+			synchronized (this) {
+				try {
+					//a thread de chegadas para até o tempo da próxima chegada
+					wait((long)tempoEntreChegadas);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			
+		}
+		//Checagem. Pode apagar depois
+		System.out.println("Tamanho da fila: " +simulador.fila.size());
+	}
+	
+}
