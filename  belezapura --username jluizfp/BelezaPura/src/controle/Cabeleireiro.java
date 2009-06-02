9d1722c6eaf8b58a796e802d5675daeaab603640
fgalluzzo <fgalluzzo@b0fe7432-4499-11de-952f-710615d9d0d5>
2009-06-02 18:57:37 +0000

    Corrigi um erro na geração do serviços. E fiz alguns acertos nas threads. Mudei a FilaEspera Ela não está ordenando pq tava com problema a ordenação, depois eu vejo isso melhor.

---  belezapura --username jluizfp/BelezaPura/src/controle/Cabeleireiro.java
+++  belezapura --username jluizfp/BelezaPura/src/controle/Cabeleireiro.java
@@ -6,73 +6,87 @@ import modelo.TipoServico;
 import view.Simulador;;
 
 public class Cabeleireiro extends Thread {
-	private Cliente c;
+	
 
 	private Servico servico_cliente;
 	private double tempo_servico;
+	private int m;
 	public void run(){
-		//Espero chegar algum cliente na fila
-		if(Simulador.n == 0){
-			try {
+		
+		 try {
 				Simulador.sinc.acquire();
-			} catch (InterruptedException e2) {
+			} catch (InterruptedException e3) {
 				// TODO Auto-generated catch block
-				e2.printStackTrace();
+				e3.printStackTrace();
 			}
-		}
+		while(true){						
+				//Espero chegar algum cliente na fila
 			
-		while(!Simulador.fila.vazia()){						
+			 Cliente c = new Cliente();
+			 c= null;
 				//semáforo de exclusão mútua para acesso a fila de espera
 				try {
 					Simulador.mutualEx.acquire();
 				
-				for(int i = 0;i<Simulador.fila.size();i++){
-					for(int j = 0;j<Simulador.fila.get(i).getServicos().size();j++){
-						if(Simulador.fila.get(i).getServicos().get(j).getTipoServico().equals(TipoServico.CORTE)){
-							c = Simulador.fila.removeCliente(i);
-							c.getServicos().remove(j);
-							Simulador.n--;
+					for(int i = 0;i<Simulador.fila.size();i++){
+						for(int j = 0;j<Simulador.fila.get(i).getServicos().size();j++){
+							if(Simulador.fila.get(i).getServicos().get(j).getTipoServico().equals(TipoServico.CORTE)){
+								c = Simulador.fila.removeCliente(i);
+								c.getServicos().remove(j);
+								Simulador.n--;
+								m =Simulador.n;
+								break;
+							}
+						}
+						if(c != null){
 							break;
 						}
 					}
-					if(c != null){
-						break;
+					
+					Simulador.mutualEx.release();
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
 					}
-				}
-				Simulador.mutualEx.release();
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-				//a thread de cabelereireiro para pelo tempo do corte do cliente
-				tempo_servico = Math.random()*Simulador.pesoCorte;
-				synchronized (this) {
+				if(c != null){
+					//a thread de cabelereireiro para pelo tempo do corte do cliente
+					tempo_servico = Math.random()*Simulador.pesoCorte;
+					synchronized (this) {
+						try {
+							
+							wait((long)tempo_servico);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					
+					//Insiro ele de volta na fila de espera
 					try {
-						
-						wait((long)tempo_servico);
+						Simulador.mutualEx.acquire();
+						Simulador.fila.insereCliente(c);
+						Simulador.mutualEx.release();
 					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}
-				
-				//Insiro ele de volta na fila de espera
-				try {
-					Simulador.mutualEx.acquire();
-					Simulador.fila.insereCliente(c);
-					Simulador.mutualEx.release();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					if(m == 0){
+						try {
+							Simulador.sinc.acquire();
+						} catch (InterruptedException e2) {
+							// TODO Auto-generated catch block
+							e2.printStackTrace();
+						}
+					}
+					//Checagem. Pode apagar depois
+					
+					System.out.println("Tamanho da fila: " +Simulador.fila.size());
+					System.out.println(currentThread().getId());
 				}
 			}
 			
 			
 			
-			//Checagem. Pode apagar depois
-		
-			System.out.println("Tamanho da fila: " +Simulador.fila.size());
-			System.out.println(currentThread().getId());
+			
 			
 		
 	}
---  belezapura --username jluizfp/BelezaPura/src/controle/ChegadaClientes.java
+++  belezapura --username jluizfp/BelezaPura/src/controle/ChegadaClientes.java
@@ -8,10 +8,11 @@ import view.Simulador;
 public class ChegadaClientes extends Thread {
 	// lambda é a taxa de clientes que chegam por segundo
 	// preferencialmente deve ser entre 0 e 1
-	private  double lambda = 0.9;
+	private  double lambda = 0.4;
+	private GeraServico gs =  new GeraServico() ;
 	
 	public void run(){
-		while((System.currentTimeMillis()- Simulador.tempoInicial) < 10000){
+		while((System.currentTimeMillis()- Simulador.tempoInicial)/1000 < 60){
 			
 			//chegadas exponencial, mas podemos usar tb o que foi especificado
 			//de chegadas entre 1 e 5 unidades de tempo
@@ -21,25 +22,25 @@ public class ChegadaClientes extends Thread {
 			Cliente c = new Cliente();
 			
 			//Atribuo serviços aleatórios ao cliente
-			c.setServicos(GeraServico.gerador());
+			c.setServicos(gs.gerador());
 			//Atribuo o tempo de chegada dele
-			c.setTempoChegada(System.currentTimeMillis() - Simulador.tempoInicial);
+			c.setTempoChegada((double)(System.currentTimeMillis() - Simulador.tempoInicial)/1000);
 			
 			//semaforo de exclusão mútua para a fila de espera
-			try {
-				Simulador.mutualEx.acquire();
+			/*try {
+				Simulador.mutualEx.acquire();*/
 				//Insiro o cliente na fila de espera
 				Simulador.fila.insereCliente(c);
 				Simulador.n++;
-				Simulador.mutualEx.release();
+				//Simulador.mutualEx.release();
 				if(Simulador.n == 1){
 					Simulador.sinc.release(3);
 				}
 			
-			} catch (InterruptedException e1) {
+			/*} catch (InterruptedException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			}
+			}*/
 				
 				
 			//Checagem. Pode apagar depois
@@ -57,7 +58,7 @@ public class ChegadaClientes extends Thread {
 			
 			
 		}
-		
+		System.out.println("Salão Fechado");
 	}
 	
 	
